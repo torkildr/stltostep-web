8421b8b73ac3e44a3e6db0e96b924363b8cf2fde
Torkild Retvedt <torkild@retvedt.no>
2022-05-31 21:36:51 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM buildpack-deps:bullseye AS builder
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+    cmake
+
+RUN git clone https://github.com/slugdev/stltostp.git \
+ && cd stltostp \
+ && cmake . \
+ && make
+
+FROM python:bullseye
+
+COPY --from=builder /stltostp/stltostp /web/stltostp
+WORKDIR /web
+EXPOSE 8000
+
+COPY server.py /web/
+ENTRYPOINT /web/server.py
+
---
+++ server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import cgi
+import shutil
+import subprocess
+import tempfile
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+stltostp="/web/stltostp"
+
+def convert(source, target):
+    try:
+        result = subprocess.run([stltostp, source, target],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+        if result.returncode == 0:
+            return (200, None)
+        else:
+            return (400, result.stderr)
+    except Exception as e:
+        return (500, str(e))
+
+class FileConvertHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_len = int(self.headers.get("Content-Length", 0))
+        if content_len == 0:
+            self.send_error(400, "Missing body")
+            return
+
+        form = cgi.FieldStorage(self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],})
+
+        if 'file' not in form:
+            self.send_error(400, "No file received")
+            return
+
+        with tempfile.NamedTemporaryFile() as stl:
+            shutil.copyfileobj(form['file'].file, stl.file)
+            self.rfile.close()
+
+            with tempfile.NamedTemporaryFile() as step:
+                (status, error) = convert(stl.name, step.name)
+
+                if error:
+                    self.send_error(status, error)
+                else:
+                    self.send_response(status)
+                    self.send_header("Content-type", "application/binary")
+                    self.end_headers()
+                    shutil.copyfileobj(step.file, self.wfile)
+
+def run(server_class=HTTPServer, handler_class=FileConvertHandler):
+    server_address = ("0.0.0.0", 8000)
+    httpd = server_class(server_address, handler_class)
+    print(f"Listening to {':'.join([str(x) for x in server_address])}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
